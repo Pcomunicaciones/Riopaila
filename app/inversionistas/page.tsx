92bfbd5303e28a552af0de62876bb9cce3cb05d0
luixmv6-eng <luixmv6@gmail.com>
2026-04-20 10:34:53 -0500

    cambio en inversionista

--- app/inversionistas/page.tsx
+++ app/inversionistas/page.tsx
@@ -410,6 +410,13 @@ const ConglomeradosView = () => (
 // --- VISTA: INFORMACIÓN RELEVANTE ---
 const InfoRelevanteView = () => {
   const hechos = [
+    {
+      f: "17/04/2026",
+      t: "Informes de fin de Ejercicio",
+      r: "En cumplimiento de lo dispuesto en las Circulares Externas 012 y 031, emitidas por la Superintendencia Financiera de Colombia, se publica el informe periódico de fin de ejercicio con corte al 31 de diciembre de 2025, aprobado por la Asamblea General de Accionistas el 25 de marzo de 2026.",
+      doc: "Riopaila- FINAL.pdf"
+    },
+
     // ================== 2026 ==================
     {
       f: "25/03/2026",
