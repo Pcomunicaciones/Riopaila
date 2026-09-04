ddd303e9e92db04e92b31d63616f719dfb56881e
WilliamPosada <wposada@agricolas.co>
2026-09-04 15:16:32 -0500

    Actualizacion de Informacion Relevante 14/08/2026 x2

--- app/inversionistas/page.tsx
+++ app/inversionistas/page.tsx
@@ -415,7 +415,7 @@ const InfoRelevanteView = () => {
       f: "14/08/2026",
       t: "Informes de fin de Ejercicio",
       r: "Se presenta el informe correspondiente al segundo trimestre de 2026, en cumplimiento de lo dispuesto en las Circulares Externas 012 de 2022 y 031 de 2021 emitidas por la Superintendencia Financiera de Colombia.",
-      doc: "Riop Inf Periodico II Trimestre 2026"
+      doc: "Riop Inf Periodico II Trimestre 2026.pdf"
     },
     //15-05-2026
     {
